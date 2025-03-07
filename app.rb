29b4fcdfe631c0e9246eba4de63acdc71f0be6af
Kevin Lavier <kevinlavier@orange.fr>
2025-03-07 18:23:27 +0100

    TEST : Add several test to find out where is the no implicit conversion hash into string

--- app.rb
+++ app.rb
@@ -7,34 +7,45 @@ set :port, 8080
 
 # POST route to create a user
 post '/users' do
-    content_type :json
-  
-    data = params
-  
-    result = User.create(
-    firstname: data['firstname'],
-    lastname:  data['lastname'],
-    age:       data['age'].to_i,
-    password:  data['password'],
-    email:     data['email']
+  content_type :json
+
+  data = if request.media_type == 'application/json'
+           request_body = request.body.read
+           JSON.parse(request_body)
+         else
+           params
+         end
+
+  puts "Parsed data: #{data.inspect}"
+
+  firstname = data['firstname'].to_s
+  lastname  = data['lastname'].to_s
+  password  = data['password'].to_s
+  email     = data['email'].to_s
+  age       = data['age'].to_i
+
+  result = User.create(
+    firstname: firstname,
+    lastname:  lastname,
+    age:       age,
+    password:  password,
+    email:     email
   )
 
-
   if result.is_a?(Hash) && result.key?(:error)
-    "Error Output: #{result}"
+    puts "Error Output: #{result}"
     return result.to_json
   end
 
-  output = { 
-    id:        result.id, 
-    firstname: result.firstname, 
-    lastname:  result.lastname, 
-    age:       result.age, 
-    email:     result.email 
+  output = {
+    id:        result.id,
+    firstname: result.firstname,
+    lastname:  result.lastname,
+    age:       result.age,
+    email:     result.email
   }
 
-  "Response Output: #{output}"
+  puts "Response Output: #{output}"
 
   output.to_json
-  end
-  
+end
